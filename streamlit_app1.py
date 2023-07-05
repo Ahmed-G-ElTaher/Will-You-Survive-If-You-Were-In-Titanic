6cb3b6a1eb897704ddd17855d17daa63233626e0
ahmedelkelany81 <124341899+Ahmed-G-ElTaher@users.noreply.github.com>
2023-07-06 01:13:11 +0300

    App

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -6,14 +6,19 @@ import seaborn as sns
 import plotly.express as px
 import joblib
 
+
 st.set_page_config(
     page_title='Will You Survive If You Were In Titanic ? :ship:',
     layout='centered',
     initial_sidebar_state='expanded'
 )
 
+
+data= pd.read_csv('Taitanic.csv') 
+
+
 st.sidebar.image('https://i.pinimg.com/564x/8a/5d/95/8a5d9586fbe7a3cbd0dad8c9c160e898.jpg')
-st.image('https://hips.hearstapps.com/hmg-prod/images/the-white-star-line-passenger-liner-r-m-s-titanic-embarking-news-photo-1608252641.?crop=1xw:0.75xh;center,top&resize=1200:*')
+st.image('https://www.financialexpress.com/wp-content/uploads/2023/06/Sub-Cover.jpg?w=580')
 st.title('Will You Survive If You Were In Titanic ? :ship:')
 
 '''
@@ -25,10 +30,6 @@ st.markdown("<br>",unsafe_allow_html=True)
     [![Account](https://camo.githubusercontent.com/a80d00f23720d0bc9f55481cfcd77ab79e141606829cf16ec43f8cacc7741e46/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4c696e6b6564496e2d3030373742353f7374796c653d666f722d7468652d6261646765266c6f676f3d6c696e6b6564696e266c6f676f436f6c6f723d7768697465)](https://www.linkedin.com/in/ahmed-el-taher/) 
 '''
 
-'''
-   
-
-'''
 st.markdown("<br>",unsafe_allow_html=True)
 '---------------------------------------------'
 st.header("Input Your Data ")
@@ -43,51 +44,69 @@ model = joblib.load('cat.joblib')
 
 columns = ['Sex','Age','Pclass','Fare','Embarked','SibSp','Parch']
 
-name  = st.text_input("Input Passenger Name", 'John Smith')
-Sex = st.selectbox("Choose sex", ['male','female'])
-if Sex == 'male' :
+name  = st.text_input("Input Your Name : ", 'John Smith')
+Sex0 = st.selectbox("Choose Your Gender : ", ['male','female'])
+if Sex0 == 'male' :
     Sex = 1
-elif Sex == 'female' :
+elif Sex0 == 'female' :
     Sex = 0
-Age = st.slider("Choose age",0,100)
-SibSp = st.slider("Choose siblings",0,8)
-Parch = st.slider("Choose parch",0,2)
-Embarked = st.selectbox("Did they Embark?", ['S','C','Q'])
-Fare = st.number_input("Input Fare Price", 0,520)
+Age = st.slider("Choose Your Age : ",0,100)
+SibSp = st.slider("How Many Siblings Will You Take With You ? ",0,8)
+Parch = st.slider("Will You Take One Of Your Parents Or Not ? ",0,2)
+Embarked = st.selectbox("Choose Your Port of Embarkation (C = Cherbourg; Q = Queenstown; S = Southampton)", ['S','C','Q'])
+
+Fare = st.number_input("Input Fare Price You Will Pay To Book A Ticket : ", 0,520)
+
+if Fare :
+    id = data[data['Fare'] >= Fare]['PassengerId'].sample(n=1).values[0]
+    passengerid = st.write("Your ID Passenger Is: ", id) 
+
+    c = 0
+    if Fare <= 150 :
+        c = 'Third Class'
+        C=3
+    elif 150 < Fare < 350 :
+        c = 'Second Class'
+        C=2
+    elif Fare >= 350 :
+        c = 'First class'
+        C=1 
+    Pclass = st.write("Your Class Is: " , c )
+
+    booking = data[data['Fare'] >= Fare]['Ticket'].sample(n=1).values[0]
+    ticket = st.write("Your Ticket Number Is: ", booking) 
 
 
-passengerid = st.write("Your ID Passenger Is: ") 
-ticket = st.text_input("Input Ticket Number", "12345") 
-c = 0
-if Fare <= 150 :
-    c = 'Third Class'
-    C=3
-elif 150 < Fare < 350 :
-    c = 'Second Class'
-    C=2
-elif Fare >= 350 :
-    c = 'First class'
-    C=1 
-Pclass = st.write("Your Class Is: " , c )
+    room = data[data['Fare'] >= Fare]['Cabin'].dropna().sample(n=1).values[0]
+    cabin = st.write("Your Cabin Is: ", room)
 
-cabin = st.text_input("Input Cabin", "C52") 
+    if Embarked == 'C' :
+        Embarked = 0
+    elif Embarked == 'S' :
+        Embarked = 2
+    else :
+        Embarked = 1
 
-if Embarked == 'C' :
-    Embarked = 0
-elif Embarked == 'S' :
-    Embarked = 2
-else :
-    Embarked = 1
+    
 
-def predict(): 
-    row = np.array([Sex,Age,Pclass,Fare,Embarked,SibSp,Parch]) 
-    X = pd.DataFrame([row], columns = columns)
-    prediction = model.predict(X)
-    if prediction[0] == 1: 
-        st.success('Passenger Survived :thumbsup:')
-    else: 
-        st.error('Passenger did not Survive :thumbsdown:') 
+    trigger = st.button('Predict Your Fate',use_container_width=True,type='primary')
 
-trigger = st.button('Predict', on_click=predict)
-
-
+    if trigger :
+        row = np.array([Sex,Age,Pclass,Fare,Embarked,SibSp,Parch]) 
+        X = pd.DataFrame([row], columns = columns)
+        prediction = model.predict(X)
+        if prediction[0] == 1: 
+                st.write('Passenger Survived :thumbsup:')
+                with st.expander("Your Fate",expanded=True):
+                    st.write("Don't worry you are in survive boat.")
+                    st.image("https://cdn.arstechnica.net/wp-content/uploads/2023/02/jack5.jpg")
+        else: 
+            st.write('Passenger did not Survive :thumbsdown:')
+            if Sex0 == 'male' :
+                with st.expander("Your Fate",expanded=True):
+                        st.write("unfortunately, you didn't Survive.")
+                        st.image("https://thumbs.gfycat.com/DifficultNimbleAntbear-size_restricted.gif")
+            else :
+                with st.expander("Your Fate",expanded=True):
+                        st.write("unfortunately, you didn't Survive.")
+                        st.image("https://s3.amazonaws.com/snwceomedia/ame-egl/ebb15fca-2426-412c-b7f2-dba39077efb2.sized-1000x1000.gif?w=1000")
